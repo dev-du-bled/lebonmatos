008a8f9416a30452849d236f7e3fbb29203640bf
Firmin_ <40271484+firminunderscore@users.noreply.github.com>
2025-11-13 10:42:45 +0100

    Added prisma.config.ts

---
+++ prisma.config.ts
@@ -0,0 +1,16 @@
+import path from "node:path";
+import { loadEnvConfig } from "@next/env";
+import { defineConfig, env } from "prisma/config";
+
+loadEnvConfig(process.cwd());
+
+export default defineConfig({
+  schema: path.join("prisma", "schema"),
+  migrations: {
+    path: path.join("prisma", "schema", "migrations"),
+  },
+  engine: "classic",
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
+});
