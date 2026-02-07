eea3b65e09cf5dbd86718453af1f6ac6549efb2f
Zigleur <eden.douru@gmail.com>
2026-02-07 14:11:07 +0100

    fix: map container index

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -15,28 +15,30 @@ export default function Map({ latitude, longitude, coordinates }: MapProps) {
     const polygonPoints = formatCoordinates(coordinates || []);
 
     return (
-        <MapContainer
-            center={[latitude, longitude]}
-            zoom={10.5}
-            scrollWheelZoom={true}
-            style={{
-                height: "400px",
-                width: "100%",
-                borderRadius: "var(--radius)",
-            }}
-        >
-            <TileLayer
-                url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
-                maxZoom={14}
-                minZoom={2}
-            />
-            {polygonPoints.length > 0 && (
-                <Polygon
-                    positions={polygonPoints}
-                    color="orange"
-                    fillOpacity={0.3}
+        <div className="relative z-0">
+            <MapContainer
+                center={[latitude, longitude]}
+                zoom={10.5}
+                scrollWheelZoom={true}
+                style={{
+                    height: "400px",
+                    width: "100%",
+                    borderRadius: "var(--radius)",
+                }}
+            >
+                <TileLayer
+                    url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    maxZoom={14}
+                    minZoom={2}
                 />
-            )}
-        </MapContainer>
+                {polygonPoints.length > 0 && (
+                    <Polygon
+                        positions={polygonPoints}
+                        color="orange"
+                        fillOpacity={0.3}
+                    />
+                )}
+            </MapContainer>
+        </div>
     );
 }
